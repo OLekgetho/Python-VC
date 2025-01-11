a4a16f13a8105375ce3d8867b5c4c86563c4cf34
Ofentse <fentselekgetho01@gmail.com>
2025-01-11 15:51:04 +0200

    Updated the content for Day2

--- main.py
+++ main.py
@@ -20,6 +20,48 @@ class Beginner:
                 count += 1
         print(count)
 
+    def evennum(self,numbers):
+        even = []
+        for x in numbers:
+            if x % 2 == 0:
+                even.append(x)
+            else:
+                continue
+        print(even)
+
+    def multiply(self,numbers):
+        curr = 1
+        for x in numbers:
+            curr = x * curr
+        print(curr)
+
+    def largest(self,numbers):
+        curr = 0
+        for x in numbers:
+            if x > curr:
+                curr = x
+            else:
+                continue
+        print(curr)
+
+    def occur(self, numbers):
+        occur = 0
+        for x in numbers:
+            if x == 3:
+                occur += 1
+            else:
+                continue
+        print(occur)
+
+    def str(self, words):
+        four = []
+        for x in words:
+            length = len(x)
+            if length > 4:
+                four.append(x)
+            else: continue
+        print(four)
+
 class Intermediate:
 
     def maximum(self,numbers):
@@ -44,6 +86,55 @@ class Intermediate:
             sum += x
             new.append(sum)
         print(new)
+
+    def reverses(self, items):
+        end = len(items) - 1
+        new = []
+        x = 0
+        while x <= end:
+            new.append(items[end])
+            end -= 1
+        print(new)
+
+    def neg(self, numbers):
+        curr = 0
+        leng = len(numbers) - 1
+        x = 0
+        while x < leng:
+            if numbers[x] < 0:
+                curr = numbers[x]
+            x += 1
+
+        print(curr)
+
+    def cumm(self,numbers):
+        curr = 1
+        leng = len(numbers) - 1
+        x = 0
+        while x <= leng:
+            curr = numbers[x] * curr
+            x += 1
+        print(curr)
+
+    def sumodd(self,numbers):
+        curr = 0
+        leng = len(numbers)-1
+        x = 0
+        while x <= leng:
+            if numbers[x] % 2 != 0:
+                curr = numbers[x] + curr
+            x += 1
+        print(curr)
+
+    def zero(self, numbers):
+        num = []
+        leng = len(numbers) - 1
+        x = 0
+        while x <= leng:
+            if numbers[x] != 0:
+                num.append(numbers[x])
+            x += 1
+        print(num)
 
 class Advanced:
 
@@ -90,15 +181,112 @@ class Advanced:
         seen.sort()
         print(seen)
 
+    def common(self, list1, list2):
+        com = []
+        for x in list2:
+            if x in list1:
+                com.append(x)
+        print(com)
+
+    def vowels(self, text):
+        new = text.lower()
+        vowels = ["a","e","i","o","u"]
+        vowelcount = 0
+        for x in new:
+            if x in vowels:
+                vowelcount += 1
+        print(vowelcount)
+
+    def square(self, numbers):
+        square = []
+        for x in numbers:
+            x **= 2
+            square.append(x)
+        print(square)
+
+    def group(self,numbers):
+        even = []
+        odd = []
+        x = 0
+        leng = len(numbers)-1
+        while x <= leng:
+            if numbers[x] % 2 == 0:
+                even.append(numbers[x])
+            else:
+                odd.append(numbers[x])
+            x += 1
+        print(even,odd)
+
+    def palindrom(self,words):
+        word = []
+        for x in words:
+            reverse = x[::-1]
+            if reverse == x:
+                word.append(x)
+            else:
+                continue
+        print(word)
+
+class Complex:
+
+    def unique(self, numbers):
+        count = 0
+        seen = []
+        for x in numbers:
+            if x not in seen:
+                seen.append(x)
+                count += 1
+            else:
+                continue
+        print(count)
+
+    def nested(self, numbers):
+        single = []
+        for x in numbers:
+            for y in x:
+                single.append(y)
+        print(single)
+
+    def freq(self,numbers):
+        seen = {}
+        for x in numbers:
+            if x not in seen:
+                seen[x] = 1
+            else:
+                seen[x] += 1
+        print(seen)
+
+    def table(self,n):
+        table = []
+        for x in range(1,11):
+            num = x * n
+            table.append(num)
+        print(table)
+
+    def element(self,items):
+        last = items[-1]
+        for x in range(len(items) -1 , 0, -1):
+            items[x] = items[x -1]
+        items[0] = last
+        print(items)
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
+    complex = Complex()
     advanced = Advanced()
-    words = ['apple', 'banana', 'cherry', 'date', 'fig', 'grape']
-    numbers = [1, 2, 3, 2, 4, 5, 3, 6, 7, 4]
-    items = [1, 2, 3, 4, 5, 93]
-    list1 = [1, 3, 5, 7, 9]
-    list2 = [2, 3, 5, 8, 10]
-    advanced.mermove(list1, list2)
+    beginner = Beginner()
+    intermediate = Intermediate()
+    items = [10, 20, 30, 40, 50]
+    numbers = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+    n = 7
+    complex.element(items)
 
     
 
